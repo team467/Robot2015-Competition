3ced1bb93d1b7863ca00cab9609ee56175b039da
npetrangelo <nathan@petrangelo.org>
2015-01-10 11:04:23 -0500

    Semi-Fixed DriverStation

--- src/org/usfirst/frc/team467/Driverstation.java
+++ src/org/usfirst/frc/team467/Driverstation.java
@@ -1,8 +1,10 @@
 package org.usfirst.frc.team467;
 
+import javax.management.RuntimeErrorException;
+
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStationLCD;
-import edu.wpi.first.wpilibj.DriverStationLCD.Line;
+//import edu.wpi.first.wpilibj.DriverStationLCD;
+//import edu.wpi.first.wpilibj.DriverStationLCD.Line;
 
 /**
  * Singleton class to handle driverstation I/O on Team467 2014 Robot
@@ -19,7 +21,7 @@ public class Driverstation
 
     //Driverstation objects
     private DriverStation driverstation;
-    private DriverStationLCD lcd;
+    // TODO private DriverStationLCD lcd;
 
     //Joystick objects
     private Joystick467 JoystickLeft;
@@ -32,7 +34,7 @@ public class Driverstation
     private Driverstation()
     {
         driverstation = DriverStation.getInstance();
-        lcd = DriverStationLCD.getInstance();
+        // TODO lcd = DriverStationLCD.getInstance();
         JoystickLeft = new Joystick467(1);
         if (NAVIGATOR_CONTROLLER_IS_GUITAR)
         {
@@ -147,38 +149,41 @@ public class Driverstation
      */
     private void printFinal(String text, int lineNum)
     {
-        //Determine Line based on given integer
-        Line line = null;
-        switch (lineNum)
-        {
-            case 1:
-                line = Line.kUser1;
-                break;
-            case 2:
-                line = Line.kUser2;
-                break;
-            case 3:
-                line = Line.kUser3;
-                break;
-            case 4:
-                line = Line.kUser4;
-                break;
-            case 5:
-                line = Line.kUser5;
-                break;
-            case 6:
-                line = Line.kUser6;
-                break;
-        }
-
-        //Return if line number is invalid
-        if (line == null)
-        {
-            return;
-        }
-
-        //Print to string buffer
-        lcd.println(line, 1, text + BLANK_LINE);
+// TODO
+    	throw new RuntimeException("TODO MIGRATION: no DriverStationLCD class");
+    	
+//        //Determine Line based on given integer
+//        Line line = null;
+//        switch (lineNum)
+//        {
+//            case 1:
+//                line = Line.kUser1;
+//                break;
+//            case 2:
+//                line = Line.kUser2;
+//                break;
+//            case 3:
+//                line = Line.kUser3;
+//                break;
+//            case 4:
+//                line = Line.kUser4;
+//                break;
+//            case 5:
+//                line = Line.kUser5;
+//                break;
+//            case 6:
+//                line = Line.kUser6;
+//                break;
+//        }
+//
+//        //Return if line number is invalid
+//        if (line == null)
+//        {
+//            return;
+//        }
+//
+//        //Print to string buffer
+//        lcd.println(line, 1, text + BLANK_LINE);
     }
 
     /**
@@ -186,7 +191,8 @@ public class Driverstation
      */
     public void sendData()
     {
-        lcd.updateLCD();
+    	throw new RuntimeException("TODO MIGRATION: no DriverStationLCD class");
+        // TODO lcd.updateLCD();
     }
 
 }
