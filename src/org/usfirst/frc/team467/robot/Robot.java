5c3407f6313b711153f225faa2dde949376dda15
Nathan Petrangelo <nathan@petrangelo.org>
2015-02-14 16:36:49 -0500

    Reformat and Added cross hairs and wheel angle monitors to Camera feed.

--- src/org/usfirst/frc/team467/robot/Robot.java
+++ src/org/usfirst/frc/team467/robot/Robot.java
@@ -39,191 +39,227 @@ public class Robot extends IterativeRobot
 
     private static final double MIN_DRIVE_SPEED = 0.1;
 
-	// Robot objects
-	private DriverStation467 driverstation;
+    // Robot objects
+    private DriverStation467 driverstation;
 
-	private Drive drive;
+    private Drive drive;
 
-	int session;
-	Image frame;
+    int session;
+    Image frame;
 
-	CameraServer cameraServer = null;
+    CameraServer cameraServer = null;
 
-	boolean useCamera = true;
-
-	/**
-	 * Time in milliseconds
-	 */
-	double time;
-
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
-
-	public class ParticleReport implements Comparator<ParticleReport>,
-			Comparable<ParticleReport> {
-		double PercentAreaToImageArea;
-		double Area;
-		double BoundingRectLeft;
-		double BoundingRectTop;
-		double BoundingRectRight;
-		double BoundingRectBottom;
-
-		public int compareTo(ParticleReport r) {
-			return (int) (r.Area - this.Area);
-		}
-
-		public int compare(ParticleReport r1, ParticleReport r2) {
-			return (int) (r1.Area - r2.Area);
-		}
-	};
-
-	// Structure to represent the scores for the various tests used for target
-	// identification
-	public class Scores {
-		double Area;
-		double Aspect;
-	};
-
-	// Images
-	Image binaryFrame;
-	int imaqError;
-
-	// Constants
-	NIVision.Range TOTE_HUE_RANGE = new NIVision.Range(101, 64); // Default hue
-																	// range for
-																	// yellow
-																	// tote
-	NIVision.Range TOTE_SAT_RANGE = new NIVision.Range(88, 255); // Default
-																	// saturation
-																	// range for
-																	// yellow
-																	// tote
-	NIVision.Range TOTE_VAL_RANGE = new NIVision.Range(134, 255); // Default
-																	// value
-																	// range for
-																	// yellow
-																	// tote
-	double AREA_MINIMUM = 0.5; // Default Area minimum for particle as a
-								// percentage of total image area
-	double LONG_RATIO = 2.22; // Tote long side = 26.9 / Tote height = 12.1 =
-								// 2.22
-	double SHORT_RATIO = 1.4; // Tote short side = 16.9 / Tote height = 12.1 =
-								// 1.4
-	double SCORE_MIN = 70.0; // Minimum score to be considered a tote
-	double VIEW_ANGLE = 49.4; // View angle fo camera, set to Axis m1011 by
-								// default, 64 for m1013, 51.7 for 206, 52 for
-								// HD3000 square, 60 for HD3000 640x480
-	NIVision.ParticleFilterCriteria2 criteria[] = new NIVision.ParticleFilterCriteria2[1];
-	NIVision.ParticleFilterOptions2 filterOptions = new NIVision.ParticleFilterOptions2(
-			0, 0, 1, 1);
-	Scores scores = new Scores();
-
-	public void robotInit() {
-		// Initialize logging framework.
-		Logging.init();
+    boolean useCamera = true;
 
     /**
      * Time in milliseconds
      */
-    double time = System.currentTimeMillis();
+    double time;
 
-		// Make robot objects
-		driverstation = DriverStation467.getInstance();
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
 
-		drive = Drive.getInstance();
-		initCamera();
+    public class ParticleReport implements Comparator<ParticleReport>, Comparable<ParticleReport>
+    {
+        double PercentAreaToImageArea;
+        double Area;
+        double BoundingRectLeft;
+        double BoundingRectTop;
+        double BoundingRectRight;
+        double BoundingRectBottom;
 
-		Calibration.init();
-	}
+        public int compareTo(ParticleReport r)
+        {
+            return (int) (r.Area - this.Area);
+        }
+
+        public int compare(ParticleReport r1, ParticleReport r2)
+        {
+            return (int) (r1.Area - r2.Area);
+        }
+    };
+
+    // Structure to represent the scores for the various tests used for target
+    // identification
+    public class Scores
+    {
+        double Area;
+        double Aspect;
+    };
+
+    // Images
+    Image binaryFrame;
+    int imaqError;
+
+    // Constants
+    NIVision.Range TOTE_HUE_RANGE = new NIVision.Range(101, 64); // Default hue
+                                                                 // range for
+                                                                 // yellow
+                                                                 // tote
+    NIVision.Range TOTE_SAT_RANGE = new NIVision.Range(88, 255); // Default
+                                                                 // saturation
+                                                                 // range for
+                                                                 // yellow
+                                                                 // tote
+    NIVision.Range TOTE_VAL_RANGE = new NIVision.Range(134, 255); // Default
+                                                                  // value
+                                                                  // range for
+                                                                  // yellow
+                                                                  // tote
+    double AREA_MINIMUM = 0.5; // Default Area minimum for particle as a
+                               // percentage of total image area
+    double LONG_RATIO = 2.22; // Tote long side = 26.9 / Tote height = 12.1 =
+                              // 2.22
+    double SHORT_RATIO = 1.4; // Tote short side = 16.9 / Tote height = 12.1 =
+                              // 1.4
+    double SCORE_MIN = 70.0; // Minimum score to be considered a tote
+    double VIEW_ANGLE = 49.4; // View angle fo camera, set to Axis m1011 by
+                              // default, 64 for m1013, 51.7 for 206, 52 for
+                              // HD3000 square, 60 for HD3000 640x480
+    NIVision.ParticleFilterCriteria2 criteria[] = new NIVision.ParticleFilterCriteria2[1];
+    NIVision.ParticleFilterOptions2 filterOptions = new NIVision.ParticleFilterOptions2(0, 0, 1, 1);
+    Scores scores = new Scores();
+
+    public void robotInit()
+    {
+        // Initialize logging framework.
+        Logging.init();
+
+        /**
+         * Time in milliseconds
+         */
+        double time = System.currentTimeMillis();
+
+        // Make robot objects
+        driverstation = DriverStation467.getInstance();
+
+        drive = Drive.getInstance();
+        initCamera();
+
+        Calibration.init();
+    }
 
     private void initCamera()
     {
-        try {
-			cameraServer = CameraServer.getInstance();
-		} catch (Exception ex) {
-			useCamera = false;
-		}
-		 cameraServer.setQuality(50);
-		
-		 frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
-		
-		 // the camera name (ex "cam0") can be found through the roborio web interface
-		 session = NIVision.IMAQdxOpenCamera("cam0",
-		 NIVision.IMAQdxCameraControlMode.CameraControlModeController);
-		 NIVision.IMAQdxConfigureGrab(session);
+        try
+        {
+            cameraServer = CameraServer.getInstance();
+        }
+        catch (Exception ex)
+        {
+            useCamera = false;
+        }
+        cameraServer.setQuality(50);
+
+        frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
+
+        // the camera name (ex "cam0") can be found through the roborio web interface
+        session = NIVision.IMAQdxOpenCamera("cam0", NIVision.IMAQdxCameraControlMode.CameraControlModeController);
+        NIVision.IMAQdxConfigureGrab(session);
     }
 
-	public void disabledInit() {
-		LOGGER.info("Robot disabled");
+    public void disabledInit()
+    {
+        LOGGER.info("Robot disabled");
 
-	}
+    }
+
+    public void disabledPeriodic()
+    {
+        renderImage();
+    }
+
+    public void autonomousInit()
+    {
+
+    }
+
+    public void teleopInit()
+    {
+
+    }
+
+    public void testInit()
+    {
+    }
+
+    public void testPeriodic()
+    {
+    }
+
+    public void autonomousPeriodic()
+    {
+        LOGGER.debug("Autonomous");
+    }
+
+    // read file in from disk. For this example to run you need to copy
+    // image.jpg from the SampleImages folder to the
+    // directory shown below using FTP or SFTP:
+    // http://wpilib.screenstepslive.com/s/4485/m/24166/l/282299-roborio-ftp
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic()
+    {
+        // Read driverstation inputs
+        driverstation.readInputs();
+
+        if (driverstation.getCalibrate())
+        {
+            // Calibrate Mode
+            Calibration.updateCalibrate();
+        }
+        else
+        {
+            // Drive Mode
+            updateDrive();
+        }
+
+        // renderImage();
+
+    }
+
+    private void renderImage()
+    {
+        int viewWidth = 640;
+        int viewHeight = 480;
+        int topMargin = 20;
+        int sideMargin = 20;
+        int rectHeight = 10;
+        int rectWidth = 100;
+
+        // Cross hair points
+        NIVision.Point vertStart = new Point(viewWidth / 2, viewHeight / 2 - 5);
+        NIVision.Point vertEnd = new Point(viewWidth / 2, viewHeight / 2 + 5);
+
+        NIVision.Point horizStart = new Point(viewWidth / 2 - 5, viewHeight / 2);
+        NIVision.Point horizEnd = new Point(viewWidth / 2 + 5, viewHeight / 2);
+
+        NIVision.Rect flRect = new NIVision.Rect(topMargin, sideMargin, rectHeight, rectWidth);
+        NIVision.Rect frRect = new NIVision.Rect(topMargin, viewWidth - (sideMargin + rectWidth), rectHeight, rectWidth);
+        NIVision.Rect blRect = new NIVision.Rect(viewHeight - (topMargin + rectHeight), sideMargin, rectHeight, rectWidth);
+        NIVision.Rect brRect = new NIVision.Rect(viewHeight - (topMargin + rectHeight), viewWidth - (sideMargin + rectWidth), 10, 100);
+
+        
+        ShapeMode shape = ShapeMode.SHAPE_RECT;
+        NIVision.IMAQdxGrab(session, frame, 1);
+        
+        // Cross Hairs
+        NIVision.imaqDrawLineOnImage(frame, frame, DrawMode.DRAW_VALUE, vertStart, vertEnd, 0.0f);
+        NIVision.imaqDrawLineOnImage(frame, frame, DrawMode.DRAW_VALUE, horizStart, horizEnd, 0.0f);
+        
+        // Angle Monitors
+        NIVision.imaqDrawShapeOnImage(frame, frame, flRect, DrawMode.DRAW_VALUE, shape, 0.0f);
+        NIVision.imaqDrawShapeOnImage(frame, frame, frRect, DrawMode.DRAW_VALUE, shape, 0.0f);
+        NIVision.imaqDrawShapeOnImage(frame, frame, blRect, DrawMode.DRAW_VALUE, shape, 0.0f);
+        NIVision.imaqDrawShapeOnImage(frame, frame, brRect, DrawMode.DRAW_VALUE, shape, 0.0f);
 
 
-	public void disabledPeriodic()
-	{
-	    renderImage();
-	}
-	
-	public void autonomousInit() {
-
-	}
-
-	public void teleopInit() {
-	    
-	}
-
-	public void testInit() {
-	}
-
-	public void testPeriodic() {
-	}
-
-	public void autonomousPeriodic() {
-		LOGGER.debug("Autonomous");
-	}
-
-	// read file in from disk. For this example to run you need to copy
-	// image.jpg from the SampleImages folder to the
-	// directory shown below using FTP or SFTP:
-	// http://wpilib.screenstepslive.com/s/4485/m/24166/l/282299-roborio-ftp
-
-	/**
-	 * This function is called periodically during operator control
-	 */
-	public void teleopPeriodic() {
-		// Read driverstation inputs
-		driverstation.readInputs();
-
-		if (driverstation.getCalibrate()) {
-			// Calibrate Mode
-			Calibration.updateCalibrate();
-		} else {
-			// Drive Mode
-			updateDrive();
-		}
-
-		// renderImage();
-
-	}
-
-	private void renderImage() {
-	    NIVision.Point start = new Point(0, 0);
-	    NIVision.Point end = new Point(640, 480);
-		/**
-		 * Rectangle to be rendered in
-		 */
-		NIVision.Rect rect = new NIVision.Rect(0, 0, 480, 640);
-		ShapeMode shape = ShapeMode.SHAPE_OVAL;
-		NIVision.IMAQdxGrab(session, frame, 1);
-		NIVision.imaqDrawShapeOnImage(frame, frame, rect, DrawMode.DRAW_VALUE,
-				shape, 0.0f);
-		NIVision.imaqDrawLineOnImage(frame, frame, DrawMode.DRAW_VALUE, start, end, 0.0f);
-
-		cameraServer.setImage(frame);
-
-	}
+        cameraServer.setImage(frame);
+    }
 
     /**
      * called once per iteration to perform any necessary updates to the drive
