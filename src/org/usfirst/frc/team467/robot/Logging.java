02f427c915022971dc12b3c8d1155e7eaeadbec8
Nathan Petrangelo <nathan@petrangelo.org>
2015-03-28 17:56:17 -0400

    Changed logging levels; added rollingFileAppender

--- src/org/usfirst/frc/team467/robot/Logging.java
+++ src/org/usfirst/frc/team467/robot/Logging.java
@@ -7,6 +7,7 @@ import org.apache.log4j.FileAppender;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
+import org.apache.log4j.RollingFileAppender;
 
 public class Logging
 {
@@ -19,10 +20,11 @@ public class Logging
 //        Logger.getLogger(Steering.class).setLevel(Level.INFO);
 //        Logger.getLogger(Calibration.class).setLevel(Level.DEBUG);
 //        Logger.getLogger(Drive.class).setLevel(Level.DEBUG);
-        Logger.getLogger(Lifter.class).setLevel(Level.INFO);
+//        Logger.getLogger(Lifter.class).setLevel(Level.DEBUG);
 //        Logger.getLogger(Claw.class).setLevel(Level.DEBUG);
-        Logger.getLogger(Autonomous.class).setLevel(Level.INFO);
+//        Logger.getLogger(Autonomous.class).setLevel(Level.DEBUG);
 //        Logger.getLogger(Gyro2015.class).setLevel(Level.DEBUG);
+        Logger.getLogger(CameraDashboard.class).setLevel(Level.DEBUG);
     }
 
     private static void setupDefaultLogging()
@@ -35,7 +37,11 @@ public class Logging
         Logger.getRootLogger().addAppender(new ConsoleAppender(layout));
         try
         {
-            Logger.getRootLogger().addAppender(new FileAppender(layout, "/tmp/Robot467.log"));
+            RollingFileAppender rollingFileAppender = new RollingFileAppender(layout, "/tmp/Robot467.log");
+            rollingFileAppender.setMaxBackupIndex(10);
+            rollingFileAppender.setMaximumFileSize(1_000_000);
+            rollingFileAppender.rollOver();
+            Logger.getRootLogger().addAppender(rollingFileAppender);
         }
         catch (IOException e)
         {
