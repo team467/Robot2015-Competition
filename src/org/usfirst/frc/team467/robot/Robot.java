b178e7c40218800671243dbed3a3aa5559ff1c04
mattnet0101 <mattnet0101@gmail.com>
2016-02-12 21:59:21 -0500

    Added support for CANTalon tank drive.

--- src/org/usfirst/frc/team467/robot/Robot.java
+++ src/org/usfirst/frc/team467/robot/Robot.java
@@ -76,7 +76,9 @@ public class Robot extends IterativeRobot
         int robotID = new DigitalInput(9).get() ? 1 : 0;
         
         if(robotID == 1) {
-            drive = TankDrive.makeTalonTank(1, 0, 2, 3);
+            //this is for the kitbot
+            //drive = TankDrive.makeTalonTank(1, 0, 2, 3);
+            drive = TankDrive.makeCANTalonTank(2, 5, 1, 6);
             LOGGER.info("Tank Set");
             
         }
